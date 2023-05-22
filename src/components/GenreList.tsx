646fd0d67b1415d9c36d94c883bab9206bf8d40e
Pau Vila <pvila89@gmail.com>
2023-05-23 00:42:05 +0200

    Fetch Games with reactQuery

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -9,7 +9,6 @@ import {
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
-import { FetchResponse } from "../hooks/useData";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import { FetchResponse } from '../services/api-client';
+import apiClient from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -16,18 +18,19 @@ export interface Game {
     rating_top: number;
   }
   
-const useGames = (gameQuery: GameQuery ) => useData<Game>('/games',  
-        [
-          gameQuery
-        ], 
-        {
-          params: {
-            genres: gameQuery.genre?.id, 
-            platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText
-          }
-        });
+const useGames = (gameQuery: GameQuery ) => 
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () => apiClient.get<FetchResponse<Game>>('games', {
+      params: {
+        genres: gameQuery.genre?.id, 
+        parent_platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
+      }
+    }).then(res => res.data)
+  })
+
    
 
 export default useGames;
